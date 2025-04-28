aa91095735d9c591932d70df89c29393ffd52e63
Pawan Singh <pawanssingh456@gmail.com>
2025-04-28 13:59:35 +0530

    removed unused route

--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,6 @@ app.use(cors());
 app.use(express.json());
 app.use("/auth", authRouter);
 app.use("/files", fileRouter);
-app.use("/uploads", express.static("uploads"));
 
 // Test route
 app.get("/", (req, res) => {
